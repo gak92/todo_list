8f8efa7ca5ff43cfd491f97df8ee53c9f8697654
gak92 <44802922+gak92@users.noreply.github.com>
2021-04-20 23:59:58 +0500

    Create About.js

---
+++ src/My Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h3>About Us </h3>
+            <p>This some about page dummy text. This some about page dummy text.
+            This some about page dummy text, This some about page dummy text.
+            </p>
+        </div>
+    )
+}
